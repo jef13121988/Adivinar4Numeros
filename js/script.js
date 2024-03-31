2b1eb790bb6f732ca4b99cc97f3b62088991482e
jef13121988 <jef_1988@hotmail.com>
2024-03-30 21:56:08 -0300

    Actualizados controles y mensajes para que sea funcional

--- js/script.js
+++ js/script.js
@@ -9,9 +9,11 @@ class ElJuego {
         this.arregloNumeros = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]; // Arreglo conteniendo los valores que puede adoptar el sorteo
         this.numeroParaAdivinar = this.crearNumero(); // Corresponde a la variable que va a contener el número a deducir/adivinar
         this.intentoActual; // Variable que contiene el intento actual a analizar
+        this.buenas; // Variable que contiene las buenas en el intento actual a analizar
+        this.regulares; // Variable que contiene las regulares en el intento actual a analizar
         this.maximoIntentos = 0; // Variable a la que se asigna el número máximo de intentos
         this.maximoTiempo = 0; // Variable a la que se le asigna el tiempo máximo para adivinar el número
-        this.cantidadDeIntentos = 0; // Contador con el número de intentos
+        // this.cantidadDeIntentos = 0; // Contador con el número de intentos
         this.intentos = []; // Arreglo que incorpora a los intentos
         this.resultadosIntentos = []; // Arreglo que incluye los resultados correspondientes a cada intento
         this.seccion = document.querySelector('#caja'); // Selecciono el contenedor que se va a modificar
@@ -129,29 +131,114 @@ class ElJuego {
         // Levanto el valor del input
         this.intentoActual = this.inputNumero.value;
 
+        // Reviso si es un número de 4 dígitos
         this.controlInput( this.intentoActual );
         
     }
 
     // Fórmula que controla que el input sea un número de 4 dígitos
     controlInput( variable ){
-        if ( this.controlNumero( variable ) ){
-            console.log("OK");
-        } else {
-            console.log("No es un número");
-        }
-        if ( this.controlLargo( variable ) ){
-            console.log("OK");
-        } else {
-            console.log("No tiene 4 dígitos");
+
+        if ( this.controlNumero( variable ) && this.controlLargo( variable ) ){
+
+            this.buenas = this.controlBuenas( this.intentoActual ); // Reviso las buenas del intento
+            this.regulares = this.controlRegulares( this.intentoActual ); // Reviso las regulares del intento
+
+            // Envío el resultado del intento al arreglo de intentos
+            this.intentos.push ( {
+                "valor": this.intentoActual,
+                "buenas": this.buenas,
+                "regulares": this.regulares
+                } );
+
+            // Genero el mensaje con los intentos
+            let mensaje = this.generarMensaje();
+
+            // Reviso si ganó
+            if ( this.buenas === 4 ){
+
+                mensaje += `¡Ganaste! <br>
+                    El número a adivinar era ${this.numeroParaAdivinar} <br>
+                    Lo sacaste en ${this.intentos.length} intentos`
+
+                // Remuevo el event listener
+                this.btnSubmitir.removeEventListener('click', this.referencia)
+
+            }
+            
+            // Inserto el mensaje generado
+            this.insertarMensaje( mensaje );      
+
         }
         
     }
 
+    // Reviso cuántos dígitos son buenos
+    controlBuenas( numero ){
+
+        let buenas = 0; // Defino la variable para contar las buenas
+
+        // Reviso los números con coincidencia
+        for ( var i = 0 ; i < 4 ; i++ ) {
+            if ( numero[i] === this.numeroParaAdivinar[i] ){
+              buenas += 1;
+            }
+
+        }
+
+        return buenas;
+
+    }
+
+    // Reviso cuántos dígitos son regulares
+    controlRegulares( numero ){
+
+        let regulares = 0; // Defino la variable para contar las buenas
+
+        // Reviso los números con coincidencia
+        for ( var i = 0 ; i < 4 ; i++ ) {
+            for ( var j = 0 ; j < 4 ; j++ ) {
+                if ( i != j ){
+                    if ( numero[i] === this.numeroParaAdivinar[j] ){
+                        regulares += 1;
+                    }
+
+                }
+
+            }
+        }
+
+        return regulares;
+
+    }
+
+    // Genero el mensaje a mostrar
+    generarMensaje(){
+        
+        let mensaje = "";
+
+        this.intentos.forEach( intento => {
+            mensaje += `${intento.valor}: Buenas: ${intento.buenas}. Regulares: ${intento.regulares}.<br>`
+            
+        });
+
+        return mensaje;
+
+    }
+
+    // Inserto el mensaje indicado
+    insertarMensaje( mensaje ){
+
+        this.cajaMensajes.innerHTML = mensaje;
+
+    }
+
     // Doy la opción de reiniciar el juego
     juegoFinalizado(){
+
         // Remuevo el Event Listener
         this.btnSubmitir.removeEventListener('click', this.referencia);
+
     }
 
 }
